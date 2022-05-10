c8ac8853d44d00ad655d349fceee889770bab0dc
Vainius <vmykolaitis1@gmail.com>
2022-05-10 12:03:16 +0300

    Tweaked routes

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,25 @@
+const isDev = process.env.NODE_ENV === 'development';
+
+/**
+ * @desc - 'titan-chest-api' in routes.titan_chest referst to the docker image
+ * titan-chest-ui docker container is on the same network as the titan-chest-api
+ * so they could communicate
+ *
+ * If you are using a non-docker version of this UI,
+ * please change 'titan-chest-api' to localhost
+ *
+ * @field titan_chest_axios - Used in axios() calls
+ * @field titan_chest - Used in general calls by fetch() and getServerSideProps()
+ * @field titan_chest_ui - Used as the UI endpoint
+ */
 const routes = {
-  titan_chest_axios: 'http://localhost:2000/api/v1',
-  titan_chest: 'http://titan-chest-api:2000/api/v1',
-  titan_chest_ui: 'http://localhost:3000'
+  titan_chest_axios: isDev
+    ? 'http://localhost:2000/api/v1'
+    : process.env.NEXT_PUBLIC_HOST_API,
+  titan_chest: isDev
+    ? 'http://titan-chest-api:2000/api/v1'
+    : process.env.NEXT_PUBLIC_HOST_API,
+  titan_chest_ui: isDev ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_HOST,
 };
 
 export const titanChest = {
@@ -9,6 +27,6 @@ export const titanChest = {
   addProject: `${routes.titan_chest}/project/add`,
   updateProject: `${routes.titan_chest}/project/update`,
   deleteProject: `${routes.titan_chest}/project/delete`,
-}
+};
 
 export default routes;
