dcf4a30d2372a6adf592982c56c5f4de630030cc
Vainius <vmykolaitis1@gmail.com>
2022-05-10 12:03:30 +0300

    Tweaked homepage to display some data

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,93 @@
+import { format } from 'date-fns';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import Container from '../components/Container';
 import Heading from '../components/Heading';
 import StatsTab from '../components/StatsTab';
+import { getBackedLiquidity, getStatsList } from '../utils/getters';
+import toCurrency from '../utils/toCurrency';
 
-export default function Home() {
+export default function Home({ titano }) {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    if (!titano.length) {
+      return;
+    }
+
+    setData(titano[0]);
+  }, [titano]);
+
   return (
     <div>
       <Head>
         <title>Titan Chest</title>
       </Head>
 
-      <main className="bg-slate-900">
+      <main className="bg-slate-900 min-h-screen">
         <Container>
-          <Heading>Titano Stats</Heading>
-          <div className="flex flex-grow flex-wrap">
-            <StatsTab value="$0,02558" name="price" />
-            <StatsTab value="$85,450,211" name="market cap" />
-            <StatsTab value="$8,524,555" name="treasury" />
-            <StatsTab value="$3,754,554" name="rfv" />
-            <StatsTab value="$6,754,554" name="liquidity" />
-            <StatsTab value="150%" name="backed liquidity" />
-            <StatsTab value="80,525" name="holders" />
-            <StatsTab value="15,587" name="average holdings" />
-            <StatsTab value="$395" name="BNB Price" />
+          <div className="flex justify-center items-center flex-col">
+            <Heading className="text-white">Titano Stats</Heading>
+            {data && (
+              <p className="mb-4 text-white text-sm">
+                Last updated:{' '}
+                <em>
+                  {format(new Date(data.created_at), 'yyy-dd-MM H:mm')} UTC+0
+                </em>
+              </p>
+            )}
           </div>
+          {data && (
+            <div className="flex flex-grow flex-wrap justify-center">
+              {data.price && (
+                <StatsTab value={toCurrency(data.price)} name="Price" />
+              )}
+
+              {data.marketcap && (
+                <StatsTab
+                  value={toCurrency(data.marketcap)}
+                  name="Market Cap"
+                />
+              )}
+
+              {data.treasury && (
+                <StatsTab value={toCurrency(data.treasury)} name="Treasury" />
+              )}
+
+              {data.rfv && <StatsTab value={toCurrency(data.rfv)} name="RFV" />}
+
+              {data.liquidity && (
+                <StatsTab value={toCurrency(data.liquidity)} name="Liquidity" />
+              )}
+
+              {data.pair_price && (
+                <StatsTab
+                  value={toCurrency(data.pair_price)}
+                  name="BNB Price"
+                />
+              )}
+
+              <StatsTab
+                value={getBackedLiquidity({
+                  rfv: data.rfv,
+                  treasury: data.treasury,
+                  liquidity: data.liquidity,
+                })}
+                name="Backed Liquidity"
+              />
+            </div>
+          )}
         </Container>
       </main>
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const titano = await getStatsList('Titano', true);
+  return {
+    props: {
+      titano,
+    },
+  };
+};
