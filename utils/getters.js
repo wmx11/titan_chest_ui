65a8312bd090aacf65fc27e5ef102ead9981669f
Vainius <vmykolaitis1@gmail.com>
2022-05-10 12:04:29 +0300

    Tweaked getters. Added isServerSide argument and a dynamic route based on that.

--- utils/getters.js
+++ utils/getters.js
@@ -1,47 +1,67 @@
 import routes from '../config/routes';
 
-export const getProjectsList = async (id = '') => {
+const getApiHost = (isServerSide = false) => {
+  return isServerSide ? process.env.HOST_API_INTERNAL : routes.titan_chest;
+};
+
+/**
+ * @param {object{}} id - ID to get
+ * @param {object{}} type - Type to get | project | liquidity | stats | bots
+ * @param {object{}} isServerSide - Is the call handled on server side
+ * @returns object{} | null;
+ */
+const fetchFromApi = async ({ id, type, isServerSide }) => {
   try {
-    const req = await fetch(`${routes.titan_chest}/project/get/${id}`);
+    const req = await fetch(`${getApiHost(isServerSide)}/${type}/get/${id}`);
     const { data } = await req.json();
     return data;
   } catch (error) {
     console.log(error);
+    return null;
   }
 };
 
-export const getAbiList = async (id = '') => {
-  const req = await fetch(`${routes.titan_chest}/abi/get/${id}`);
-  const { data } = await req.json();
+export const getProjectsList = async (id = '', isServerSide) => {
+  const data = await fetchFromApi({ id, isServerSide, type: 'project' });
   return data;
 };
 
-export const getNetworksList = async (id = '') => {
-  const req = await fetch(`${routes.titan_chest}/network/get/${id}`);
-  const { data } = await req.json();
+export const getAbiList = async (id = '', isServerSide) => {
+  const data = await fetchFromApi({ id, isServerSide, type: 'abi' });
   return data;
 };
 
-export const getStatsList = async (id = '') => {
-  const req = await fetch(`${routes.titan_chest}/stats/get/${id}`);
-  const { data } = await req.json();
+export const getNetworksList = async (id = '', isServerSide) => {
+  const data = await fetchFromApi({ id, isServerSide, type: 'network' });
   return data;
 };
 
-export const getTokensList = async (id = '') => {
-  const req = await fetch(`${routes.titan_chest}/token/get/${id}`);
-  const { data } = await req.json();
+export const getStatsList = async (id = '', isServerSide) => {
+  const data = await fetchFromApi({ id, isServerSide, type: 'stats' });
   return data;
 };
 
-export const getLiquidityList = async (id = '') => {
-  const req = await fetch(`${routes.titan_chest}/liquidity/get/${id}`);
-  const { data } = await req.json();
+export const getTokensList = async (id = '', isServerSide) => {
+  const data = await fetchFromApi({ id, isServerSide, type: 'token' });
   return data;
 };
 
-export const getBotsList = async (id = '') => {
-  const req = await fetch(`${routes.titan_chest}/bots/get/${id}`);
-  const { data } = await req.json();
+export const getLiquidityList = async (id = '', isServerSide) => {
+  const data = await fetchFromApi({ id, isServerSide, type: 'liquidity' });
   return data;
+};
+
+export const getBotsList = async (id = '', isServerSide) => {
+  const data = await fetchFromApi({ id, isServerSide, type: 'bots' });
+  return data;
+};
+
+export const getBackedLiquidity = ({ rfv, treasury, liquidity }) => {
+  if (!rfv && !liquidity && !treasury) {
+    return null;
+  }
+
+  const assets = rfv + treasury;
+  const backedLiquidity = (assets * 100) / liquidity;
+  return `${backedLiquidity.toFixed()}%`;
 };
