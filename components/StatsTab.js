eb502e5be16d5fc85571507b02dba573b7f4d390
Vainius <vmykolaitis1@gmail.com>
2022-05-10 12:02:51 +0300

    Some style tweaks

--- components/StatsTab.js
+++ components/StatsTab.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function StatsTab({ name, value }) {
   return (
-    <div className="cursor-pointer border-titano-pink border-2 rounded-md flex-grow md:flex-grow-0 w-1/2 md:w-1/3 lg:w-1/6 py-7 flex flex-col items-center mr-4 mb-4 shadow-lg shadow-titano-pink/20">
-      <p className="text-titano-green drop-shadow-[0px_0px_4px_rgb(0,253,213)] text-xl">
+    <div className="border-titano-pink border-2 rounded-md flex-grow md:flex-grow-0 w-full md:w-1/3 lg:w-1/6 py-7 flex flex-col items-center md:mr-4 mb-4 shadow-lg shadow-titano-pink/30">
+      <p className="text-titano-green drop-shadow-[0px_0px_3px_rgb(0,253,213)] text-xl">
         {value}
       </p>
       <p className="text-slate-100 text-sm">{name}</p>
