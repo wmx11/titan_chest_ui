3c6c258942ac6009dad40f5233d3fce18fea9ac9
Vainius <vmykolaitis1@gmail.com>
2022-05-10 22:29:01 +0300

    Added maxDigits to toCurrency

--- utils/toCurrency.js
+++ utils/toCurrency.js
@@ -1,11 +1,13 @@
-const toCurrency = (number) => {
+const toCurrency = (number, maxDigits = 3) => {
   if (!number) {
     return;
   }
-  
+
   return number.toLocaleString('en-US', {
     style: 'currency',
     currency: 'USD',
+    minimumSignificantDigits: 2,
+    maximumSignificantDigits: maxDigits,
   });
 };
 
