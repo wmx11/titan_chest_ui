803ac3de83dea23f01d1f94491c4a7749123e597
Vainius <vmykolaitis1@gmail.com>
2022-05-27 23:21:20 +0300

    Refactored Aside component. Added mobile support.

--- components/Layouts/titanchest/Aside.js
+++ components/Layouts/titanchest/Aside.js
@@ -1,32 +1,38 @@
 import React, { useState } from 'react';
-import MenuLink from './MenuLink';
+import { Drawer } from '@mantine/core';
+import MenuLinksGroup from './MenuLinksGroup';
 
 function Aside() {
-  const [isOpen, setIsOpen] = useState(true);
-
-  const handleOnClick = () => {
-    setIsOpen(!isOpen);
-  };
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div
-      className={`backdrop-blur-3xl bg-slate-800/50 min-h-screen px-3 py-6 hidden md:block shadow-xl shadow-titano-green/10 relative z-10 ${
-        isOpen ? 'w-60' : 'w-20'
-      }`}
+      className={`fixed bottom-0 w-full backdrop-blur-3xl bg-slate-900/60 shadow-xl shadow-titano-green/10 z-20 px-4 py-2 pb-0 md:bg-slate-800/50 md:px-3 md:py-6 md:relative md:min-h-screen md:block md:w-60`}
     >
-      {/* <button
-        className="bg-red-100 absolute top-0 right-0 cursor-pointer z-10"
-        onClick={handleOnClick}
-      >
-        Close
-      </button> */}
-      <div className="px-4">
-        <p className="text-white mb-4 text-2xl">Titan Chest</p>
+      <div className="px-4 hidden md:block">
+        <p className="text-white mb-4 text-2xl cursor-default select-none">
+          Titan Chest
+        </p>
       </div>
       <div>
-        <ul className="text-xs">
-          <MenuLink href="/">Home</MenuLink>
-          <MenuLink href="/trading-chart">Trading Chart</MenuLink>
+        <ul className="text-sm flex items-center justify-between md:block">
+          <MenuLinksGroup
+            onBurgerClick={() => setIsOpen(true)}
+            opened={isOpen}
+            isDrawer={false}
+          />
+          <Drawer
+            opened={isOpen}
+            onClose={() => setIsOpen(false)}
+            position="bottom"
+            size="50%"
+            classNames={{
+              drawer: 'backdrop-blur-md bg-slate-900/60 px-2',
+              closeButton: 'hidden',
+            }}
+          >
+            <MenuLinksGroup isDrawer={true} />
+          </Drawer>
         </ul>
       </div>
     </div>
