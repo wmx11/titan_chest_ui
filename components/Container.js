7b3d1a579d16720206d4c91db4c47c2feb65ea3e
Vainius <vmykolaitis1@gmail.com>
2022-05-30 20:46:15 +0300

    Added ref to the Container element

--- components/Container.js
+++ components/Container.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
-function Container({ children, className }) {
+const Container = forwardRef(({ children, className }, ref) => {
   return (
-    <div className={`container mx-auto p-4 ${className && className}`}>
+    <div
+      className={`container mx-auto p-4 ${className && className}`}
+      ref={ref}
+    >
       {children}
     </div>
   );
-}
+});
+
+Container.displayName = 'Container';
 
 export default Container;
