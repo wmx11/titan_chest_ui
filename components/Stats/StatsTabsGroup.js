90e870c0d74ad3985e2d75cd4dc8c371f5b16a34
Vainius <vmykolaitis1@gmail.com>
2022-05-30 12:07:38 +0300

    Added a compute field to stats tab type

--- components/Stats/StatsTabsGroup.js
+++ components/Stats/StatsTabsGroup.js
@@ -6,36 +6,38 @@ import StatsTab from './StatsTab';
 function StatsTabsGroup({ data, lastDayData, onChartSelect }) {
   return (
     data &&
-    statsTemplate.map(({ entry, name, currency, value, tooltip }, index) => {
-      if (data[entry] && entry !== false) {
-        return (
-          <StatsTab
-            key={`${entry}_${name}_${index}`}
-            type={entry}
-            name={name}
-            dayChange={(data[entry] / lastDayData[entry] - 1) * 100}
-            value={
-              currency !== false
-                ? toCurrency(data[entry], data[entry].toFixed(3).length - 1)
-                : data[entry].toLocaleString()
-            }
-            tooltip={tooltip}
-            onChartSelect={onChartSelect}
-          />
-        );
-      }
+    statsTemplate.map(
+      ({ entry, name, currency, value, tooltip, compute }, index) => {
+        if (data[entry] && entry !== false) {
+          return (
+            <StatsTab
+              key={`${entry}_${name}_${index}`}
+              type={compute || entry}
+              name={name}
+              dayChange={(data[entry] / lastDayData[entry] - 1) * 100}
+              value={
+                currency !== false
+                  ? toCurrency(data[entry], data[entry].toFixed(3).length - 1)
+                  : data[entry].toLocaleString()
+              }
+              tooltip={tooltip}
+              onChartSelect={onChartSelect}
+            />
+          );
+        }
 
-      if (entry === false && value) {
-        return (
-          <StatsTab
-            key={`${entry}_${name}_${index}`}
-            name={name}
-            value={value(data)}
-            tooltip={tooltip}
-          />
-        );
+        if (entry === false && value) {
+          return (
+            <StatsTab
+              key={`${entry}_${name}_${index}`}
+              name={name}
+              value={value(data)}
+              tooltip={tooltip}
+            />
+          );
+        }
       }
-    })
+    )
   );
 }
 
