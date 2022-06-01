7d59339df3fba66610377bf7db3c39b1be6f9c63
Vainius <vmykolaitis1@gmail.com>
2022-06-01 16:28:51 +0300

    Added roadmap boilerplate

---
+++ config/roadmap.js
@@ -0,0 +1,32 @@
+const roadmap = [
+  {
+    title: 'Some Title',
+    completed: false,
+    description: 'Some Description',
+    position: 0,
+    enabled: true,
+  },
+  {
+    title: 'Some Title',
+    completed: false,
+    description: 'Some Description',
+    position: 2,
+    enabled: true,
+  },
+  {
+    title: 'Some Title',
+    completed: false,
+    description: 'Some Description',
+    position: 4,
+    enabled: true,
+  },
+  {
+    title: 'Some Title',
+    completed: false,
+    description: 'Some Description',
+    position: 3,
+    enabled: true,
+  },
+];
+
+export default roadmap;
