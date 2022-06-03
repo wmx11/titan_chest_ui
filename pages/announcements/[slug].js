320fd67870c10b66ac2170b8b12da322e5f016d8
Vainius <vmykolaitis1@gmail.com>
2022-06-03 20:48:13 +0300

    Added announcement page

---
+++ pages/announcements/[slug].js
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React from 'react';
+import Container from '../../components/Container';
+import GoBack from '../../components/GoBack';
+import Heading from '../../components/Heading';
+import Layout from '../../components/Layouts/titanchest/Layout';
+import Post from '../../components/Layouts/titanchest/Post';
+import useCmsPost from '../../hooks/cms/useCmsPost';
+
+function Announcement() {
+  const router = useRouter();
+  const { slug } = router.query;
+  const { data } = useCmsPost('announcements', slug);
+
+  return (
+    <div>
+      <Head>
+        <title>{data && data.title}</title>
+      </Head>
+      <Layout>
+        <Container>
+          <GoBack />
+          {data && (
+            <>
+              <Heading className="text-white">{data.title}</Heading>
+              <Post data={data} isSummary={false} />
+            </>
+          )}
+        </Container>
+      </Layout>
+    </div>
+  );
+}
+
+export default Announcement;
