9674b384269581f04d49b12d61c273b1f9d2c112
Vainius <vmykolaitis1@gmail.com>
2022-06-07 15:13:53 +0300

    Added neon text pink color

--- components/NeonText.js
+++ components/NeonText.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-function NeonText({ children }) {
+function NeonText({ children, className, type = 'green' }) {
+  const shadowType =
+    type === 'green'
+      ? 'drop-shadow-[0px_0px_3px_rgb(0,253,213)]'
+      : 'drop-shadow-[0px_0px_4px_rgb(220,2,223)]';
   return (
-    <div className="text-titano-green drop-shadow-[0px_0px_3px_rgb(0,253,213)] text-xl tracking-wider break-words">
+    <div
+      className={`text-titano-${type} ${shadowType} text-xl tracking-wider break-words ${className}`}
+    >
       {children}
     </div>
   );
