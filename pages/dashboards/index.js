bafb9d8d1d57752f8b6f2acc0a0b0fa0f7075189
Vainius <vmykolaitis1@gmail.com>
2022-06-10 20:47:06 +0300

    Added link to holders dashboard

--- pages/dashboards/index.js
+++ pages/dashboards/index.js
@@ -1,3 +1,4 @@
+import { UserGroupIcon } from '@heroicons/react/solid';
 import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
@@ -70,9 +71,42 @@ function Index() {
                     <>
                       <p>Track Titano Key Performance Indicators.</p>
                       <strong>
-                        You can see the changes in Titano performance based on This, last week, and this month.
+                        You can see the changes in Titano performance based on
+                        This, last week, and this month.
                       </strong>
-                      <p>Compare data and the impact of major announcements/releases.</p>
+                      <p>
+                        Compare data and the impact of major
+                        announcements/releases.
+                      </p>
+                    </>
+                  }
+                />
+              </a>
+            </Link>
+            <Link href="/dashboards/holders">
+              <a className="md:w-[49%]">
+                <DarkCard
+                  Head={
+                    <NeonCardWrapper className="bg-titano-pink/5 flex-grow">
+                      <NeonText>
+                        <UserGroupIcon className="w-5 h-5 animate-pulse" />
+                      </NeonText>
+                      <NeonText>Holders</NeonText>
+                      <SmallText>Titano holders dashboard</SmallText>
+                    </NeonCardWrapper>
+                  }
+                  Body={
+                    <>
+                      <p>View all wallets holding Titano.</p>
+                      <strong>
+                        You can see all wallets distribution by token amount.
+                        There is also a wallet tier list that corresponds to the
+                        given token amount in the wallet. The tier list is not
+                        official but a community made listing.
+                      </strong>
+                      <p>
+                        Filter wallets by size, order. Find wallets by address.
+                      </p>
                     </>
                   }
                 />
