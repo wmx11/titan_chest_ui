7405876f0eb2d0a7196f4fa3027988a528b1bbe0
Vainius <vmykolaitis1@gmail.com>
2022-06-18 23:42:22 +0300

    Added popover tooltip to the stats tab

--- components/Stats/StatsTab.js
+++ components/Stats/StatsTab.js
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
 import { TrendingUpIcon, TrendingDownIcon } from '@heroicons/react/outline';
-import {
-  InformationCircleIcon,
-  PresentationChartLineIcon,
-} from '@heroicons/react/solid';
-import { Popover } from '@mantine/core';
+import { PresentationChartLineIcon } from '@heroicons/react/solid';
+
 import NeonCardWrapper from '../NeonCardWrapper';
 import NeonText from '../NeonText';
 import SmallText from '../SmallText';
+import PopoverTooltip from '../Layouts/titanchest/PopoverTooltip';
 
 function StatsTab({ name, value, dayChange, tooltip, type, onChartSelect }) {
-  const [opened, setOpened] = useState(false);
-
   return (
     value && (
       <NeonCardWrapper>
@@ -39,34 +35,7 @@ function StatsTab({ name, value, dayChange, tooltip, type, onChartSelect }) {
           </div>
         )}
         <div className="absolute bottom-1 right-0 flex justify-between w-full px-2">
-          <div className="text-slate-100 text-xs mt-1">
-            <Popover
-              opened={opened}
-              target={
-                <InformationCircleIcon
-                  className="h-6 w-6 md:h-4 md:w-4 cursor-pointer"
-                  onClick={() => setOpened(true)}
-                />
-              }
-              onClose={() => setOpened(false)}
-              width={260}
-              withArrow
-              position="top-start"
-              withCloseButton
-              styles={{
-                body: {
-                  background: '#DC02DF',
-                  fontWeight: 'bold',
-                  borderColor: '#DC02DF',
-                  color: '#fff',
-                },
-                arrow: { background: '#DC02DF', borderColor: '#DC02DF' },
-                close: { color: '#0F172A' },
-              }}
-            >
-              {tooltip}
-            </Popover>
-          </div>
+          <PopoverTooltip tooltip={tooltip} />
           {dayChange !== undefined && (
             <div className="text-slate-100 text-xs mt-1">
               <PresentationChartLineIcon
